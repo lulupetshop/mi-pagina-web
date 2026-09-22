44e59cc0889a12e784107a6cc6f87a56b4d0b62c
lulupetshop <lulu.lulupetshop@gmail.com>
2026-09-22 17:23:05 -0300

    Integra Meta Pixel y eventos de conversión

--- js/app.js
+++ js/app.js
@@ -151,6 +151,34 @@
         window.dataLayer.push(Object.assign({ event: name }, params || {}));
       }
     } catch (e) {}
+    try {
+      if (typeof window.fbq === "function") {
+        const metaParams = params || {};
+        const metaEvents = {
+          view_item: "ViewContent",
+          add_to_cart: "AddToCart",
+          begin_checkout: "InitiateCheckout",
+          click_whatsapp: "Contact",
+          generate_lead: "Lead",
+        };
+        const metaName = metaEvents[name];
+        if (metaName) window.fbq("track", metaName, metaParams);
+      }
+    } catch (e) {}
+  }
+
+  function setupMetaPixel() {
+    const id = CONFIG?.ANALYTICS?.META_PIXEL_ID;
+    if (!analyticsIdValido(id, "") || !/^\\d{5,20}$/.test(String(id).trim())) return;
+    if (typeof window.fbq !== "function") {
+      !(function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)})(window,document,"script",
+      "https://connect.facebook.net/en_US/fbevents.js");
+    }
+    window.fbq("init", String(id).trim());
+    window.fbq("track", "PageView");
   }
 
   function setupAnalytics() {
@@ -1468,6 +1496,7 @@
   /* ---------- Inicio ---------- */
   function init() {
     setupAnalytics();
+    setupMetaPixel();
     setupContexto();
     setupPromocion();
     setupDialogs();
